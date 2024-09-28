fcffc03b14a86bb9fdb84c1fa3fe0bf5a50c3ee5
sam geyer <geyerwork@gmail.com>
2024-09-28 22:17:42 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import requests
+
+def main():
+    print("Welcome to the Spotify to Apple Music migration tool! More functionality will be added shortly")
+    # We'll add more functionality here later
+
+if __name__ == "__main__":
+    main()
